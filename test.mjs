664565afbcace9e163e8657f8a3fee9c2a553423
pH <1430310+pH200@users.noreply.github.com>
2023-04-11 08:18:03 +0800

    fix test

--- test.mjs
+++ test.mjs
@@ -1,9 +1,9 @@
-import { sharp } from 'sharp'
+import sharp from 'sharp'
 // Generate an image from text
-sharp({
+await sharp({
   text: {
     text: 'Hello, world!',
     width: 400, // max width
     height: 300 // max height
   }
-}).toFile('text_bw.png').done()
+}).toFile('text_bw.png')
